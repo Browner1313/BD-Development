5b070c765b14c32611c02960443f1483e2094623
Kaden Brown <ktbrown013@gmail.com>
2025-01-02 15:41:14 -0500

    Changes

--- bd-fishing/server/main.lua
+++ bd-fishing/server/main.lua
@@ -1,10 +1,10 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- In server/main.lua
+
 QBCore.Functions.CreateCallback('bd-fishing:getPlayerXP', function(source, cb)
   local playerId = source
-  -- Retrieve XP from database
-  local xp = 0  -- Placeholder for actual database retrieval logic
+  
+  local xp = 0  
   cb(xp)
 end)
 
